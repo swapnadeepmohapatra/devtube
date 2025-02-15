78cb901c37bfb18a7f49cc10e471ac44b2835788
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2025-02-15 17:21:49 +0530

    [Added] error in frontend

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -9,11 +9,61 @@ function Home() {
   const { videos } = useContext(VideoContext);
 
   return (
-    <div className={styles.videoGrid}>
-      {videos.map((video) => (
-        <VideoElement key={video._id} video={video} />
-      ))}
-    </div>
+    <>
+      <div className={styles.videoGrid}>
+        {videos.map((video) => (
+          <VideoElement key={video._id} video={video} />
+        ))}
+      </div>
+      {videos.length === 0 && (
+        <div className={styles.noVideos}>
+          <h1>No videos found</h1>
+          <br />
+          <h2>
+            The background services like OpenSearch, CloudFront CDN and other
+            services might have been disabled due to high costs. Please contact
+            the developer to enable the services.
+          </h2>
+          <br />
+          <h3>
+            The developer can be contacted via the{" "}
+            <a
+              href="mailto:hello@swapnadeep.com"
+              style={{
+                color: "rgb(3, 196, 184)",
+              }}
+            >
+              email
+            </a>
+          </h3>
+          <br />
+          <h3>
+            You can visit the GitHub repository{" "}
+            <a
+              href="https://github.com/swapnadeepmohapatra/devtube"
+              style={{
+                color: "rgb(3, 196, 184)",
+              }}
+            >
+              here
+            </a>
+          </h3>
+          <br />
+          <br />
+          <br />
+          <h3>
+            <a
+              href="https://swapnadeep.com"
+              style={{
+                color: "rgb(3, 196, 184)",
+              }}
+            >
+              View the developer&#39;s website
+            </a>
+          </h3>
+        </div>
+      )}
+    </>
   );
 }
 
